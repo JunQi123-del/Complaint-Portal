d6fbd6f97b1d378ec93726b2de8ba4efc2c2cf1b
JunQi123-del <76680865+JunQi123-del@users.noreply.github.com>
2021-06-20 11:33:28 +0800

    mysql database file

---
+++ ticketapp/app/Models/InternalComment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InternalComment extends Model
+{
+    use HasFactory;
+}
