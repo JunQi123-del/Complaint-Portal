5d2e17f389e51cd17c70319d2635d14ed7503ead
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-25 00:44:23 +0800

    Update DepartmentAccount.php

--- ticketapp/app/Models/DepartmentAccount.php
+++ ticketapp/app/Models/DepartmentAccount.php
@@ -7,5 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class DepartmentAccount extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'id',     // department account id
+        'depart_acc_name',
+        'depart_acc_pwd',
+        'depart_acc_email',
+        'depart_id'
+    ] ;
+
+    public function tickets ()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function department ()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
