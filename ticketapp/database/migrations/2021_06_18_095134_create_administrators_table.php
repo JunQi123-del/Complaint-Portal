c580e133fd8dbf63230aa8a088d99ca6a9ae991d
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-25 00:44:52 +0800

    Create 2021_06_18_095134_create_administrators_table.php

---
+++ ticketapp/database/migrations/2021_06_18_095134_create_administrators_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Administrator extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administrators',function(Blueprint $table){
+        $table->bigIncrements('id');    // admin id
+        $table->string('admin_name');
+        $table->string('admin_pwd');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropifExists('administrators');
+        //
+    }
+}
