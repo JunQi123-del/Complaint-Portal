5f9bf4db3e5c9362484b3421cc0236066e1b6602
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-24 16:22:13 +0800

    Update Ticket.php

--- ticketapp/app/Models/Ticket.php
+++ ticketapp/app/Models/Ticket.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
+    // HasFactory allows you to build fake data for your models. 
+    // It is very useful for testing and seeding fake data into 
+    // your database to see your code in action before any real
+    // user data comes in. 
     use HasFactory;
 }
