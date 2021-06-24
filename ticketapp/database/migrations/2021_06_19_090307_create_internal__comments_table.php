1c21afacfa8ad65e54d058b75747e6fd56169b61
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-25 00:45:32 +0800

    Create 2021_06_19_090307_create_internal__comments_table.php

---
+++ ticketapp/database/migrations/2021_06_19_090307_create_internal__comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class InternalComment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('internal_comments',function(Blueprint $table){
+            $table->bigIncrements('id');    // internal comment id
+            $table->timestamps();
+            $table->text('internal_comment');
+            $table->string('attatchment');
+            $table->unsignedBigInteger('depart_acc_id');
+            $table->unsignedBigInteger('admin_id');
+            $table->foreign('depart_acc_id')->references('id')->on('department_accounts');
+            $table->foreign('admin_id')->references('id')->on('administrators');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropifExists('internal_comments');
+    }
+}
