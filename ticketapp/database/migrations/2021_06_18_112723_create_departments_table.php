77eab3d9cbcee2f180d6b4dc65355531425e5001
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-25 00:44:56 +0800

    Create 2021_06_18_112723_create_departments_table.php

---
+++ ticketapp/database/migrations/2021_06_18_112723_create_departments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Department extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments',function(Blueprint $table){
+            $table->bigIncrements('id');    // department id
+            $table->string('depart_name');
+            $table->unsignedBigInteger('admin_id');
+            $table->foreign('admin_id')->references('id')->on('administrators');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropifExists('departments');
+    }
+}
