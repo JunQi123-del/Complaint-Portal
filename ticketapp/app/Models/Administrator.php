b05672bac24835c32bdd4c0a123f409c3468285c
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-25 00:44:01 +0800

    Update Administrator.php

--- ticketapp/app/Models/Administrator.php
+++ ticketapp/app/Models/Administrator.php
@@ -7,5 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Administrator extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'id',
+        'A_name',
+        'A_accPwd'
+    ] ;
+
+
+    public function tickets ()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function comments ()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function internalComments ()
+    {
+        return $this->hasMany(InternalComment::class);
+    }
+
+    public function deparments ()
+    {
+        return $this->hasMany(Departmen::class);
+    }
+ 
 }
