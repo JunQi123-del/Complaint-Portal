32bde602912229ac0b45ff7b92e7cc41cc9cbc76
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-24 16:30:01 +0800

    Update web.php

--- ticketapp/routes/web.php
+++ ticketapp/routes/web.php
@@ -18,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
     return view('welcome');
 });
+
+Route::get('/createTicket', function(){
+    return view('ticket.create');
+});
