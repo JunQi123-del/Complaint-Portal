fb6d886b9fae15c5dfe6e64d41d9135dc5dc99b0
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-25 00:44:28 +0800

    Update InternalComment.php

--- ticketapp/app/Models/InternalComment.php
+++ ticketapp/app/Models/InternalComment.php
@@ -7,5 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class InternalComment extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'id',    // internal comment id
+        'internal_comment',
+        'attatchment',
+        'depart_acc_id',
+        'admin_id'
+    ] ;
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    public function departmentAccount ()
+    {
+        return $this->belongsTo(DepartmentAccount::class);
+    }
+
+    public function administrator ()
+    {
+        return $this->belongsTo(Administrator::class);
+    }
 }
