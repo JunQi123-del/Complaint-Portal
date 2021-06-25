bf655eec8de5150c1072762bc696663ae5366b4d
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-26 01:20:55 +0800

    Update 2021_06_18_095134_create_administrators_table.php

--- ticketapp/database/migrations/2021_06_18_095134_create_administrators_table.php
+++ ticketapp/database/migrations/2021_06_18_095134_create_administrators_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Administrator extends Migration
+
+class CreateAdministratorsTable extends Migration
 {
     /**
      * Run the migrations.
