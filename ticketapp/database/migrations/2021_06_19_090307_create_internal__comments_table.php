691a29d6514c4e1255c71b6f59e872ed41bd073c
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-26 01:21:17 +0800

    Update 2021_06_19_090307_create_internal__comments_table.php

--- ticketapp/database/migrations/2021_06_19_090307_create_internal__comments_table.php
+++ ticketapp/database/migrations/2021_06_19_090307_create_internal__comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class InternalComment extends Migration
+class CreateInternalCommentsTable extends Migration
 {
     /**
      * Run the migrations.
