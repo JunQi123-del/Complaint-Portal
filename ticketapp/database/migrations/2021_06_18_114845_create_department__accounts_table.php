68a86d8f25b16010631ebbb8cbe85236d42ced50
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-26 01:21:05 +0800

    Update 2021_06_18_114845_create_department__accounts_table.php

--- ticketapp/database/migrations/2021_06_18_114845_create_department__accounts_table.php
+++ ticketapp/database/migrations/2021_06_18_114845_create_department__accounts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DepartmentAccount extends Migration
+class CreateDepartmentAccountsTable extends Migration
 {
     /**
      * Run the migrations.
