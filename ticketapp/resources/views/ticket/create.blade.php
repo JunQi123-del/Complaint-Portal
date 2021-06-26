b0e77c1998cd27209e2ebcd43e3245ccfcd9f276
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-26 17:00:00 +0800

    Update create.blade.php

--- ticketapp/resources/views/ticket/create.blade.php
+++ ticketapp/resources/views/ticket/create.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.layout')
 
 @section('content')
+<br><br><br><br>
+<div class="container">
+    <h1>Create Ticket(for now)Hello</h1>
+    <div>
+    {!! Form::open(['action' => 'TicketController@store', 'method' => 'POST']) !!}
+        <div class="form-group">
+            {{Form::label('title', 'Title')}}
+            {{Form::text('title', '', ['class' => 'form-control'])}}
+        </div>
+
+    {!! Form::close() !!}
+
+    </div>
+</div>
+
 
 @endsection
