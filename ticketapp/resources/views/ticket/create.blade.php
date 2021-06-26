ca8c8df8fb9eab5497ff20e97aa27a0cce12e4d8
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-06-27 00:35:30 +0800

    Update create.blade.php

--- ticketapp/resources/views/ticket/create.blade.php
+++ ticketapp/resources/views/ticket/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 
 @section('content')
-<br><br><br><br>
+<br><br><br><br><br><br>
 <div class="container">
     <h1>Create Ticket(for now)Hello</h1>
     <div>
@@ -10,11 +10,16 @@
             {{Form::label('title', 'Title')}}
             {{Form::text('title', '', ['class' => 'form-control'])}}
         </div>
-
+        <div class="form-group">
+            {{Form::label('body', 'Body')}}
+            {{Form::textarea('body', '', ['id' => 'wysiwyg-editor', 'class' => 'form-control' , 'enctype' => 'multipart/form-data'])}}
+        </div>
+        {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
     {!! Form::close() !!}
 
     </div>
 </div>
+
 
 
 @endsection
