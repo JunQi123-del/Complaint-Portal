6a6d2ac96d3bdbda16a00abd33075f6cbdbdae37
JunQi123-del <76680865+JunQi123-del@users.noreply.github.com>
2021-06-29 21:58:18 +0800

    Added admin table views
    Register button not working yet

--- ticketappV2/resources/views/dashboards/admins/allaccounts.blade.php
+++ ticketappV2/resources/views/dashboards/admins/allaccounts.blade.php
@@ -2,8 +2,44 @@
 @section('title','All Accounts')
 @section('header','All Accounts')
 
+@section('register')
+<button type="button" class="btn btn-primary">Register</button>
+@endsection
+
+
+
+
 @section('contents')
 
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">role</th>
+      <th scope="col">Created at</th>
+      <th scope="col">Updated at</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($allAccounts as $row)
+    <tr>
+        <td>{{$row['id']}}</td>
+        <td>{{$row['name']}}</td>
+        <td>{{$row['email']}}</td>
+        @if($row->role == '1')
+        <td>Administrator</td>
+        @else
+        <td>Department</td>
+        @endif
+        <td>{{$row['created_at']}}</td>
+        <td>{{$row['updated_at']}}</td>
+    </tr>
+
+    @endforeach
+  </tbody>
+</table>
 
 
 
--- ticketappV2/resources/views/dashboards/admins/layouts/admin-dash-layout.blade.php
+++ ticketappV2/resources/views/dashboards/admins/layouts/admin-dash-layout.blade.php
@@ -264,10 +264,8 @@
             <h1 class="m-0">@yield('header')</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="{{ route('register') }}">@yield('register')</a></li>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
@@ -275,8 +273,8 @@
     <!-- /.content-header -->
 
     <!-- Main content -->
-    
-     @yield('contents');
+
+     @yield('contents')
 
 
   <!-- /.content-wrapper -->
