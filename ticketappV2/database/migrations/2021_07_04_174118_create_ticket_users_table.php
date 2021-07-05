14fc2c239fe8acde157292778fb4f33c98b81ddf
Jord-Edu <81552834+Jord-Edu@users.noreply.github.com>
2021-07-05 11:52:59 +0800

    Create 2021_07_04_174118_create_ticket_users_table.php

---
+++ ticketappV2/database/migrations/2021_07_04_174118_create_ticket_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_users', function (Blueprint $table) {
+            $table->unsignedInteger('ticket_id');
+            $table->unsignedInteger('user_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket_users');
+    }
+}
