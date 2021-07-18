bd03460d138d76603e97b9ad551931ded91cf3dc
JunQi123-del <76680865+JunQi123-del@users.noreply.github.com>
2021-07-18 20:46:09 +0800

    changed the layout of the admin and user dashboard

--- ticketappV2/resources/views/dashboards/admins/dashboard.blade.php
+++ ticketappV2/resources/views/dashboards/admins/dashboard.blade.php
@@ -185,34 +185,7 @@
           <!-- ./col -->
         </div>
         <!-- /.row -->
-        <!-- Main row -->
-        <div class="row">
-          <!-- Left col -->
-          <section class="col-lg-7 connectedSortable">
-            <!-- Custom tabs (Charts with tabs)-->
-            <div class="card card-success">
-              <div class="card-header">
-                <h3 class="card-title">Bar Chart</h3>
-
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button>
-                </div>
-              </div>
-              <div class="card-body">
-                <div class="chart">
-                  <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-                </div>
-              </div>
-              <!-- /.card-body -->
-            </div>
-
-    
-</section>
+       
 </section>
 
 
